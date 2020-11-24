36d1be19f852c7d4d5f6999db9678afa79f8252c
Let's Explore <35136064+BShaila@users.noreply.github.com>
2020-11-24 15:31:28 +0530

    First Update
    Code Updated

---
+++ pollster/polls/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Question, Choice 
+from django.template import loader 
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+# Get qusetions and display them
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]#-pub_date here '-' means 
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+# Show specific question and choices
+def detail(request, question_id):
+    try :
+        question = Question.objects.get(pk=question_id)
+    except QuestionDoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+
+# Get question and display results
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try :
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the qusetion voting form
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice."
+        })
+    else :
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
